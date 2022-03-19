923dc2f17426b4b60a6b4bbafdcf8d221bda3a5a
ADEPOJU007 <ibrahimadepojuu@gmail.com>
2022-03-19 16:08:14 -0700

    low

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
-#include "holberton.h"
+#include <stdio.h>
 #include "main.h"
 
 /**
  * print_alphabet - Print the alphabet in lowercase.
  *
- * Return: void.*
+ * Return: void.
  */
 
 void print_alphabet(void)
